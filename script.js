665d915a2b847a34c466cbebc5f08942230c3ef4
Jakeline Martines <jakeline_13_martines@hotmail.com>
2023-10-10 19:59:26 +0100

    function js
    Add function to login

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const loginForm = document.getElementById('loginForm');
-    
-    loginForm.addEventListener('submit', function (event){
-        event.preventDefault();
+function login(){
+    var login = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
 
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
-
-        console.log('E-mail:' + email);
-        console.log('Password:' + password)
-    });
-});
+    if(login == 'admin' && password == 'admin')
+        alert('sucess');
+}
